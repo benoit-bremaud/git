20e890017747265135120fcf45eecde6944bd952
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-09-14 22:46:34 +0200

    chore: Update Python application structure template
    - Create a structured Python file (code.py) with comments to help organize code.
    - This serves as a template for organizing Python applications.

---
+++ guizero/00_.py
@@ -0,0 +1,26 @@
+# Imports ----------------
+# Importez ici les modules nécessaires pour votre application.
+
+# Constants --------------
+# Définissez ici des constantes ou des variables globales si nécessaire.
+
+# Functions --------------
+# Déclarez ici les fonctions que vous utiliserez dans votre application.
+
+# GUI Setup ---------------
+# Initialisez ici votre interface graphique en utilisant une bibliothèque comme Tkinter, PyQt, guizero, etc.
+
+# Widgets ----------------
+# Créez ici les éléments graphiques (fenêtres, boutons, zones de texte, etc.) de votre application.
+
+# Event Handlers ---------
+# Définissez ici les gestionnaires d'événements pour réagir aux actions de l'utilisateur.
+
+# Main Code ---------------
+# Placez ici le code principal de votre application.
+
+# Display ----------------
+# Affichez ici l'interface graphique ou lancez votre application.
+
+# End of File ------------
+# C'est la fin du fichier. Vous pouvez ajouter d'autres sections si nécessaire.
